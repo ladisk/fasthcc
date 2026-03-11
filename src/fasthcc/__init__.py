4d7397521318e831dc957ef54ad7bb56a57c891a
jasasonc <jasasonc@gmail.com>
2026-03-11 11:08:10 +0100

    Bump version to 0.2.0

--- src/fasthcc/__init__.py
+++ src/fasthcc/__init__.py
@@ -1,5 +1,5 @@
 from fasthcc.reader import read_hcc, HCCReader
 from fasthcc.writer import write_hcc, HCCWriter
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 __all__ = ["read_hcc", "HCCReader", "write_hcc", "HCCWriter", "__version__"]
